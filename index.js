81e7d8d171d873d34b098f7d2e9a13149a0bdd5a
joeyshi07 <joeys19@nycstudents.net>
2024-10-24 13:58:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function factorial(n) {
+/* function factorial(n) {
   if (n < 1) {
     return; // guard clause
   }
@@ -21,13 +21,29 @@ function checkSpaces(n, dayOne, dayTwo) {
     }
   }
   return count;
-}
-console.log(checkSpaces(5, "CC..C", ".CC.C"));
+/* */
 
 //strings --> bits and bytes - values btwn 0 and 255 -- bits either 0 or 1
 //in binary
 // from 2^0 to 2^7 all 1s --> add to 255
 // hexadecimal in base 16 0 to f --> larger range
+
+function decimalToBinary(n) {
+  let binary = "";
+  let i = 0;
+  while (i <= 7) {
+    if (n >= 2 ** (7 - i)) {
+      binary += "1";
+      n -= 2 ** (7 - i);
+    } else {
+      binary += "0";
+    }
+    i += 1;
+  }
+  return binary;
+}
+console.log(decimalToBinary(5));
+console.log(decimalToBinary(123));
 
 //if larger than 128, subtract 2^7 and append 1. do the same with the rest of the powers of 2 until 2^0, where if the number is larger than the valuye of the power, you subtract it from the number and append 1 and if not append 0 and skip to the next digit.
 // for the first index, if it equals 1, append 2^7 otherwise skip to the next. iteratre through the whole thing, subtracting one from the power each time. if it equals 1, add it on to the previous appended until everything gets run through.
